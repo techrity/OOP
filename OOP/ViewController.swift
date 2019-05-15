088ac0506d63a7172816943950f4b8062a7d5587
Prayudi <prayudisatriyo@icloud.com>
2019-05-15 08:14:12 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Prayudi Satriyo on 15/05/19.
+//  Copyright © 2019 Prayudi Satriyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
